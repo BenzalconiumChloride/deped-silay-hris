89b73b1dea6d58c03b69db20829e50b8834896fc
benzalconiumChloride <lozada.benz@outlook.com>
2026-02-09 13:53:55 +0800

    v1-beta

--- employees/index.php
+++ employees/index.php
@@ -1,25 +1,28 @@
-<?php
-require_once '../../global-library/database.php';
-
-if (isset($_GET['news-and-updates'])) {
-    $view = 'news';
-} else {
-    $view = '';
-}
-
-$currentPage = 'news';
-
-switch ($view) {
-    case 'news':
-        $content   = 'news.php';
-        $pageTitle = 'news';
-        break;
-
-    default:
-        $content   = 'news.php';
-        $pageTitle = 'news';
-        break;
-}
-
-require_once '../include/template.php';
+<?php
+require_once '../global-library/database.php';
+
+if (isset($_GET['employees'])) {
+    $view = 'Employees';
+} else {
+    $view = '';
+
+}
+
+$currentPage = 'Employees';
+
+switch ($view) {
+
+    case 'employees':
+            $content   = 'employees.php';
+            $pageTitle = 'Employees';
+        break;
+
+    default:
+            $content   = 'employees.php';
+            $pageTitle = 'Employees';
+        break;
+}
+
+require_once '../include/template.php';
+
 ?>
